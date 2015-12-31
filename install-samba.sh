e2c0571246074a6a10d612d3e2a1802661594d93
luniun <luniun@icloud.com>
2015-12-31 02:11:55 +0100

    Update install-samba.sh

--- install-samba.sh
+++ install-samba.sh
@@ -1,3 +1,6 @@
+echo "Montage de sda2 dans /home/nas"
+mkdir -p /home/nas
+mount /dev/sda2 /home/nas
 echo "Installation de Samba"
 apt-get install samba samba-common libcups2 -y
 mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
@@ -8,3 +11,20 @@ echo "Entrer le mot de passe pour l'utilisateur annette"
 adduser annette
 echo "Entrer le mot de passe pour l'utilisateur medias"
 adduser medias
+cp smb.conf /etc/samba/
+echo "Creation des utilisateur Samba"
+echo "Creation de luniun"
+smbpasswd luniun
+echo "Creation de annette"
+smbpasswd annette
+mkdir -p /home/nas/luniun
+chmod 700 /home/nas/luniun
+chown luniun:luniun /home/nas/luniun
+mkdir -p /home/nas/annette
+chmod 700 /home/nas/annette
+chown annette:annette /home/nas/annette
+mkdir -p /home/nas/medias
+chmod 777 /home/nas/medias
+echo "Redemarage de Samba"
+/etc/init.d/samba restart
+echo "Installation Terminer!"
