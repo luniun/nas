7d894aad52df73585671f85a5913e1e6278a9e17
root <root@OXNAS>
2015-12-31 09:42:45 +0800

    ajout de install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+echo "Installation de samba"
+chmod +x install-samba.sh
+./install-samba.sh
+echo "Installation de mini dlna"
+pause
+chmod +x install-dlna.sh
+./install-dlna.sh
+echo "Installation de ajenti"
+pause
+chmod +x install-ajenti.sh
+./install-ajenti.sh
+echo "Installation Terminer"
+
